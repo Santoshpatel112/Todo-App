ee430385fcebe9750c7e296054085a4944d255d4
Santosh Patel <santoshpatelvns5@gmail.com>
2026-01-19 12:40:00 +0530

    page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import ConnectDb from "../lib/db.js"
 export default async function Home() {
   const conn=await ConnectDb();
-  console.log(conn);
+  console.log("Db Connected Sucessfully");
   return (
     <>
     <Button>Welcome to TODO-app</Button>
